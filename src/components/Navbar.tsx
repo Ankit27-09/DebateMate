e01d3d44978839480c275da465fb133fc521633e
aditi424 <aditigopinath2@gmail.com>
2025-05-12 01:21:31 +0530

    Added new pages on the navbar and linked it respectively

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -104,16 +104,7 @@ export default function Navbar() {
 
           {/* CTA Buttons */}
           <div className="flex items-center space-x-4">
-          <Button
-              variant="ghost"
-              size="icon"
-              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-              className="mr-2"
-            >
-              <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-              <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-              <span className="sr-only">Toggle theme</span>
-          </Button>
+          
             <SignedOut>
               {/* Sign In / Sign Up buttons for signed-out users */}
               {!isMobile && (
